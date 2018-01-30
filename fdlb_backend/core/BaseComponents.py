f2ebf1f18a15f1591b2808585327a78482f1b2e1
Yura Braiko <yura.braiko@raccoongang.com>
2018-01-30 20:57:52 +0200

    starting dashboard

---
+++ fdlb_backend/core/BaseComponents.py
@@ -0,0 +1,43 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class BaseTag(ABC):
+
+    @property
+    @abstractmethod
+    def name(self) -> str:
+        pass
+
+    @property
+    @abstractmethod
+    def display_name(self) -> str:
+        pass
+
+
+class BaseSnippet(ABC):
+
+    @property
+    @abstractmethod
+    def name(self) -> str:
+        pass
+
+    @property
+    @abstractmethod
+    def content_url(self) -> str:
+        pass
+
+    @property
+    @abstractmethod
+    def tags(self) -> List[BaseTag]:
+        pass
+
+    @property
+    @abstractmethod
+    def display_name(self) -> str:
+        pass
+
+    @property
+    @abstractmethod
+    def description(self) -> str:
+        pass
