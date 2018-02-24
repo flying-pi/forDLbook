1e1529e0e45cb7f4a06921a5af50b4f62d8af965
Yura Braiko <yura.braiko@raccoongang.com>
2018-02-24 11:33:12 +0200

    add matrix by matrix mull

--- fdlb_backend/base_math/snippets/matrix_operations.py
+++ fdlb_backend/base_math/snippets/matrix_operations.py
@@ -19,12 +19,12 @@ class MatrixScalarMull(BaseSnippet):
                 .add(ScalarView(view_id='scalar'))
                 .add(ButtonView(view_id='calculate', value=_('Calculate!!'), on_submit=self.on_submit))
                 .add(LabelView(view_id='result_label', value=_('Result'), visible=False))
-                .add(MatrixView(view_id='result_matrix', visible=False))
+                .add(MatrixView(view_id='result_matrix', visible=False, editable=False))
         )
 
     @property
     def content_url(self) -> str:
-        return reverse('base_math:create_user')
+        return reverse('base_math:matrix_scalar_mull')
 
     @property
     def tags(self) -> List[BaseTag]:
@@ -32,28 +32,37 @@ class MatrixScalarMull(BaseSnippet):
 
     @property
     def display_name(self) -> str:
-        return u'Множення матриць на скаляр'
+        return _(u'Множення матриць на скаляр')
 
     @property
     def description(self) -> str:
-        return u'Множення матриць на скаляр'
+        return _(u'Множення матриць на скаляр')
 
     def on_submit(self):
         scalar = self.scalar.value
         self.result_matrix.value = [[c * scalar for c in r] for r in self.matrix.value]
         self.result_matrix.visible = True
-        self.result_matrix.editable = False
         self.result_label.visible = True
 
 
 class MatrixByMatrixMull(BaseSnippet):
+
     @property
     def layout(self) -> SimpleLayout:
-        return SimpleLayout()
+        return (
+            SimpleLayout(view_id='root')
+                .add(LabelView(value=_('Matrix mull by matrix')))
+                .add(MatrixView(view_id='matrix_a'))
+                .add(LabelView(value=_('mull by:')))
+                .add(MatrixView(view_id='matrix_b'))
+                .add(ButtonView(view_id='calculate', value=_('Calculate!!'), on_submit=self.on_submit))
+                .add(LabelView(view_id='result_label', value=_('Result'), visible=False))
+                .add(MatrixView(view_id='result_matrix', visible=False, editable=False))
+        )
 
     @property
     def content_url(self) -> str:
-        return 'example.com'
+        return reverse('base_math:matrix_matrix_mull')
 
     @property
     def tags(self) -> List[BaseTag]:
@@ -61,8 +70,28 @@ class MatrixByMatrixMull(BaseSnippet):
 
     @property
     def display_name(self) -> str:
-        return u'Множення матриць на матрицю'
+        return _(u'Множення матриць на матрицю')
 
     @property
     def description(self) -> str:
-        return u'Множення матриць на матрицю'
+        return _(u'Множення матриць на матрицю')
+
+    def on_submit(self):
+        a = self.matrix_a.value
+        b = self.matrix_b.value
+
+        error_message = None
+        if len(a) == 0 or len(a[0]) == 0 or len(b) == 0 or len(b[0]) == 0:
+            error_message = _("Can not mull zero matrix")
+        elif len(a[0]) != len(b):
+            error_message = _("Count of the column first matrix must be equal to count of the rows second matrix")
+
+        if error_message is not None:
+            self.result_label.visible = True
+            self.result_label.value = error_message
+            return
+
+        result = [[sum([a[i][r]*b[r][j] for r in range(len(b))]) for j in range(len(b[0]))] for i in range(len(a))]
+        self.result_matrix.value = result
+        self.result_matrix.visible = True
+        self.result_label.visible = True
--- fdlb_backend/base_math/urls.py
+++ fdlb_backend/base_math/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 
-from base_math.snippets.matrix_operations import MatrixScalarMull
+from base_math.snippets.matrix_operations import MatrixScalarMull, MatrixByMatrixMull
 
-urlpatterns = ([
-                   url(r'^matrix_scalar_mull', MatrixScalarMull.as_view(), name='create_user')
-               ], 'base_math')
+urlpatterns = (
+    [
+        url(r'^matrix_scalar_mull', MatrixScalarMull.as_view(), name='matrix_scalar_mull'),
+        url(r'^matrix_matrix_mull', MatrixByMatrixMull.as_view(), name='matrix_matrix_mull')
+    ], 'base_math')
