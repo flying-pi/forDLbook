1bb04a0a850133048a86e8645c2b8b31e3b4b00a
Yura Braiko <yura.braiko@raccoongang.com>
2018-03-22 12:39:21 +0200

    Add model for the saving theta.

--- fdlb_backend/supervisor_neural_network/models.py
+++ fdlb_backend/supervisor_neural_network/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from django.db.models import Model, TextField, BinaryField, FloatField
+
+
+class WeightModel(Model):
+    name = TextField(default='unknown')
+    shape = BinaryField()
+    body = BinaryField()
+    accuracy = FloatField(default=-1)
--- fdlb_backend/supervisor_neural_network/snippets/simple_network_creator.py
+++ fdlb_backend/supervisor_neural_network/snippets/simple_network_creator.py
@@ -8,6 +8,7 @@ from django.utils.translation import ugettext_lazy as _
 from core.base_components import BaseSnippet, BaseTag, SimpleLayout
 from core.models import RawUserFile
 from core.snippets_view.simple_view import LabelView, ScalarView, UploadFile, ButtonView
+from supervisor_neural_network.models import WeightModel
 
 
 class SimpleNeuralNetworkCreator(BaseSnippet):
@@ -16,10 +17,12 @@ class SimpleNeuralNetworkCreator(BaseSnippet):
     def layout(self) -> SimpleLayout:
         return (
             SimpleLayout(view_id='root')
+                .add(LabelView(value=_('NN Name')))
+                .add(ScalarView(view_id='nn_name', value='NN'))
                 .add(LabelView(value=_('Enter network shape in next format :: `number neuron on first layer '
                                        'layer;number neuron on second hiden layer;....;number neuron on output '
                                        'layer`')))
-                .add(ScalarView(view_id='matrix_shape', value='784;60;10'))
+                .add(ScalarView(view_id='matrix_shape', value='784;100;60;10'))
                 .add(LabelView(value=_('label column name')))
                 .add(ScalarView(view_id='label_column_name', value='label'))
                 .add(UploadFile(view_id='train_data'))
@@ -59,7 +62,7 @@ class SimpleNeuralNetworkCreator(BaseSnippet):
             espect: np.ndarray,
             theta: List[np.ndarray],
             lambda_value=0.1,
-            learning_rate=0.1
+            learning_rate=0.3
     ):
         a = input
         m = input.shape[0]
@@ -74,8 +77,8 @@ class SimpleNeuralNetworkCreator(BaseSnippet):
         cost = (sum((np.log(a[i].transpose()).dot(espect[i])) + (np.log(1 - a[i].transpose()).dot((1 - espect[i])))
                     for i in range(a.shape[1])) / (-m) +
                 sum(np.sum(t ** 2) for t in theta) * lambda_value / (2 * m))
-        delta = [(a-espect)]
-        for i in reversed(range(1,len(theta))):
+        delta = [(a - espect)]
+        for i in reversed(range(1, len(theta))):
             layer_z[i] = np.insert(layer_z[i], layer_z[i].shape[1], 1, axis=1)
             d = (delta[0].dot(theta[i]))
             d = d * self.sigmoid_delta(layer_z[i])
@@ -84,7 +87,7 @@ class SimpleNeuralNetworkCreator(BaseSnippet):
         for i in range(len(theta)):
             d = delta[i]
             grad = d.T.dot(layer_a[i]) / m
-            grad[:,1:] = grad[:,1:] + (1*lambda_value / m * (theta[i][:,1:]))
+            grad[:, 1:] = grad[:, 1:] + (1 * lambda_value / m * (theta[i][:, 1:]))
             theta[i] = theta[i] - (grad * learning_rate)
         # https://github.com/nex3z/machine-learning-exercise/blob/master/coursera-machine-learning-python/ex4/nn_cost_function.py
         return cost
@@ -115,29 +118,43 @@ class SimpleNeuralNetworkCreator(BaseSnippet):
                 continue
             output = self.transform_results(group['label'].values)
             input = group.loc[:, group.columns != self.label_column_name.value].values
+            input = input / 255.0 * 2.0 - 1.0
             result.append({'input': input, 'output': output})
         return result
 
-    def init_theta(self):
-        given_layer_shape = list(map(lambda x: int(x), self.matrix_shape.value.split(';')))
-        return [
-            np.random.rand(given_layer_shape[i], given_layer_shape[i - 1] + 1)*2-1
-            for i in range(1, len(given_layer_shape))
-        ]
+    def get_shape(self) -> np.ndarray:
+        return np.array(list(map(lambda x: int(x), self.matrix_shape.value.split(';'))))
+
+    def init_theta(self, shape: np.ndarray):
+        return np.array([np.random.rand(shape[i], shape[i - 1] + 1) * 2 - 1 for i in range(1, len(shape))])
 
     def on_learn(self):
         if not self.train_data.value:
             # show some error message
             pass
 
+        # data = self.read_data(self.train_data.value, group_shape=[0.8, 0.01, 0.2])
         data = self.read_data(self.train_data.value)
-        theta = self.init_theta()
+        shape = self.get_shape()
+        theta = self.init_theta(shape)
         train = data[0]
         test = data[2]
-        for i in range(5000):
+        estimation = 0
+        for i in range(6):
             cost = self.train(train['input'], train['output'], theta)
-            print("cost: %s",cost)
-            if i%5 == 0:
+            print("cost: %s", cost)
+            if i % 5 == 0:
                 estimation = self.estimate(test['input'], test['output'], theta)
                 print("estimation: %s", estimation)
+        original_name = self.nn_name.value
+        name = original_name
+        i = 1
+        while WeightModel.objects.filter(name=name).exists():
+            name = original_name + str(i)
+            i += 1
+        serialized_shape = shape.tobytes()
+        serialized_theta = theta.tobytes()
+        WeightModel.objects.create(name=name, shape=serialized_shape, body=serialized_theta, accuracy=estimation).save()
+        self.learning_accuracy.value = str(estimation)
+        self.nn_name.value = name
         a = 0
